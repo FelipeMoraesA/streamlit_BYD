a433ea0915dff59e1bcfb6debcf48711ff07163e
Felipe <131537147+FelipeMoraesA@users.noreply.github.com>
2023-11-26 18:24:05 -0300

    Add files via upload

---
+++ byd.py
@@ -0,0 +1,46 @@
+
+import streamlit as st
+import json
+
+st.set_page_config(
+    page_title = 'Descubra a BYD',
+    page_icon = 'https://www.byd.com.br/wp-content/uploads/2020/08/cropped-logo-BYD-new-2022-small-90x57.png',
+    layout = 'wide'
+)
+
+st.title('Do you love BYD?')
+st.write('Conheca as caracteristicas de cada veiculo')
+
+c1, c2 = st.columns([1, 1])
+
+with c1:
+    model = st.selectbox(
+        label = 'Escolha um Modelo:',
+        options = ['BYD SEAL', 'BYD DOLPHIN', 'BYD YUAN PLUS', 'BYD SONG PLUS', 'BYD TAN', 'BYD HAN']
+    )
+
+    with open('byd.json', 'r') as arquivo_json:
+        dados = json.load(arquivo_json)
+
+    for i in dados:
+        if i['car'] == model.lower():
+            st.write(f'Modelo da linha "**{i["line"]}**", sua propulsao e **{i["prop"]}**;')
+            st.write(f'Faz de 0 a 100 km/h em **{i["acceleration_0_100"]}** segundos;')
+            st.write(f'Possui **{i["colors"]}** cores disponiveis;')
+            price = i['price']
+            price = '{:,.2f}'.format(price).replace(',','*').replace('.', ',').replace('*', '.')
+            st.write(f'Preco: **R$ {price}**.')
+            url_img = i['url_img']
+            url_pdf = i['pdf']
+    
+    st.write(f'Veja a ficha completa em [pdf]({url_pdf}).')
+    
+with c2:
+    st.image(
+        url_img, 
+        caption = 'Imagem meramente ilustrativa',
+        width = 800
+    )
+
+st.write('---')
+st.markdown(f''''<center>{'{:-^100}'.format('Saiba os beneficios da spirulina clicando [AQUI](https://www.scielo.br/j/rbme/a/Z8PKY9Vrmf98Lhj8PhmMdcS/?lang=pt)')}</center>''', unsafe_allow_html = True)
