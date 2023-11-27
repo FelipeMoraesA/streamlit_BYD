593b97e81af9f30042af81542a042ee39e2bbdff
Felipe <131537147+FelipeMoraesA@users.noreply.github.com>
2023-11-27 13:49:40 -0300

    2

--- byd.py
+++ byd.py
@@ -26,7 +26,7 @@ def displayPDF(file):
         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
 
     # Embedding PDF in HTML
-    return f'''<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="600" type="application/pdf"></iframe>'''
+    return f'''<embed src="data:application/pdf;base64,{base64_pdf}" width="100%" height="600" type="application/pdf">'''
 
 css = '''
 <style>
