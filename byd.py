c8d6499b64fd018144e5edbf7d6b7f49b8cf8d74
Felipe <131537147+FelipeMoraesA@users.noreply.github.com>
2023-11-29 13:33:09 -0300

    .

--- byd.py
+++ byd.py
@@ -69,9 +69,9 @@ with c1:
 
     pv_min = price * 0.2
     
-    st.write(f'O valor mínimo para a entrada é R$ {pv_min:.2f} (20%)')
-    pv = st.number_input('Entrada', 1, 5000, 250)
+    pv = st.number_input('Entrada (Mín. 20%)', 1, 5000, 250)
     n = st.slider('Nº de Parcelas', 1, 36, 12, 1)
+    i = st.slider('Taxa de Juros', 0.1, 50.0, 1, 1)
            
 with c2:
     tab1, tab2, tab3 = st.tabs(['Imagem', 'Vídeo', 'Ficha Técnica'])
